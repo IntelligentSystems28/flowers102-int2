2ea4d9d4e497eea35571f7d33d7dc824aab39bab
OwenTho <owen_thomas@btinternet.com>
2023-05-09 02:07:18 +0100

    Changed the model.

--- model_flower.py
+++ model_flower.py
@@ -9,34 +9,64 @@ class FlowerModel(nn.Module):
         super(FlowerModel, self).__init__()
         self.layer1 = nn.Sequential(
             nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
+
             nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
+
             nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
+
             nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
+            nn.BatchNorm2d(64),
             nn.AvgPool2d(kernel_size=2),
+            nn.ReLU(),
+
             nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
+
             nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(128),
             nn.ReLU(),
+
             nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(128),
-            nn.ReLU()
+            nn.AvgPool2d(kernel_size=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
         )
 
         self.fc = nn.Sequential(
             nn.Flatten(),
-            nn.Dropout(0.2),
+            nn.Dropout(0.5),
             # first linear must be image size * image size * last Conv2d out channel
-            nn.Linear(14 * 14 * 128, 1024),
+            nn.Linear(1 * 1 * 256, 2048),
+
+            nn.BatchNorm1d(2048),
+            nn.ReLU(),
+            nn.Linear(2048, 1024),
+
+            nn.Dropout(0.5),
             nn.ReLU(),
             nn.Linear(1024, 512),
+
+            nn.BatchNorm1d(512),
             nn.ReLU(),
             nn.Linear(512, num_classes),
             nn.LogSoftmax(dim=1)
